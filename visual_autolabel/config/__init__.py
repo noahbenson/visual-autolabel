70a78ea5bb176d9569fc9b0c43e599d5eeb14324
noahbenson <nben@uw.edu>
2024-06-18 11:14:52 -0700

    moving code around

--- visual_autolabel/config/__init__.py
+++ visual_autolabel/config/__init__.py
@@ -6,7 +6,9 @@
 """Global configuration variables for the `visual_autolabel` package.
 
 The `visual_autolabel.config` package contains definitions of global variables
-that are used throughout the package.
+that are used throughout the package. See also the 
+`visual_autolabel.benson2024.config` package for configuration items that were
+used in the paper by Benson et al. (2024).
 
 Attributes
 ----------
@@ -23,32 +25,16 @@ default_partition : tuple
     in the package is `(0.8, 0.2)`, indicating that 80% of subjects should be
     in the training dataset, and 20% of the subjects should be in the valudation
     dataset.
-sids : NumPy array of ints
-    The subject IDs of the HCP subjects used in the training datasets
 """
 
 from ._core import (
     saved_image_size,
     default_image_size,
     default_partition,
-    sids,
-    nyusids
 )
-
-# We also have three variables defined only here: model and data cache paths.
-# You can overwrite these to change the value that None gets converted into
-# when passed for either the model_cache_path or data_cache_path options (which
-# are occasionally abbreviated just cache_path) are given.
-model_cache_path = None
-data_cache_path = None
-nyudata_cache_path = None
 
 __all__ = [
     "saved_image_size",
     "default_image_size",
     "default_partition",
-    "sids",
-    "model_cache_path",
-    "data_cache_path",
-    "nyudata_cache_path"
 ]
--- visual_autolabel/config/_core.py
+++ visual_autolabel/config/_core.py
@@ -4,11 +4,6 @@
 # Configuration of the visual_autolabel package.
 # This file just contains global definitions that are used throughout.
 
-import warnings
-
-import numpy as np
-import neuropythy as ny
-
 
 #-------------------------------------------------------------------------------
 # saved_image_size
@@ -31,25 +26,3 @@ default_image_size = 128
 # second.
 default_partition = (0.8, 0.2)
 
-#-------------------------------------------------------------------------------
-# sids
-# The subject-IDs that we use in training.
-if len(ny.data['hcp_lines'].exclusions) == 0:
-    warnings.warn('neuropythy "hcp_lines" dataset is missing exclusions')
-sids = np.array([sid for sid in ny.data['hcp_lines'].subject_list
-                 if ('mean',sid,'lh') not in ny.data['hcp_lines'].exclusions
-                 if ('mean',sid,'rh') not in ny.data['hcp_lines'].exclusions])
-sids.flags['WRITEABLE'] = False
-nyusids = np.array(
-    ['sub-wlsubj001', 'sub-wlsubj004', 'sub-wlsubj006', 'sub-wlsubj007',
-     'sub-wlsubj014', 'sub-wlsubj019', 'sub-wlsubj023', 'sub-wlsubj042',
-     'sub-wlsubj043', 'sub-wlsubj045', 'sub-wlsubj046', 'sub-wlsubj055',
-     'sub-wlsubj056', 'sub-wlsubj057', 'sub-wlsubj062', 'sub-wlsubj064',
-     'sub-wlsubj067', 'sub-wlsubj071', 'sub-wlsubj076', 'sub-wlsubj079',
-     'sub-wlsubj081', 'sub-wlsubj083', 'sub-wlsubj084', 'sub-wlsubj085',
-     'sub-wlsubj086', 'sub-wlsubj087', 'sub-wlsubj088', 'sub-wlsubj090',
-     'sub-wlsubj091', 'sub-wlsubj092', 'sub-wlsubj094', 'sub-wlsubj095',
-     'sub-wlsubj104', 'sub-wlsubj105', 'sub-wlsubj109', 'sub-wlsubj114',
-     'sub-wlsubj115', 'sub-wlsubj116', 'sub-wlsubj117', 'sub-wlsubj118',
-     'sub-wlsubj120', 'sub-wlsubj122', 'sub-wlsubj126'])
-nyusids.flags['WRITEABLE'] = False
