020c5b565471f3ac0bd264770ebac63c9439d457
noahbenson <nben@uw.edu>
2025-03-19 09:35:15 -0700

    adding 3D data management file

--- src/visual_autolabel/image/__init__.py
+++ src/visual_autolabel/image/__init__.py
@@ -15,6 +15,11 @@ from ._data import (
     LabelIntersectFeature,
     NullFeature
 )
+from ._data3D import (
+    dataset3D_cache_path_init as dataset3D_cache_path,
+    noddi_data_path_init as noddi_data_path,
+    _sids as sids
+)
 from ._model import (
     UNet2D,
     UNet3D,
