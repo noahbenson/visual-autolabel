150194821d1cfe601eadf3532263412c64f4b500
noahbenson <nben@uw.edu>
2025-12-10 09:35:57 -0800

    adding placeholder module for hybrid project code

---
+++ src/hybrid/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# visual_autolabel/hybrid/__init__.py
+# Code for the project running hybrid 2D/3D models on the HCP.
+
+
+
